dc716432a08cd4df78aa84b1266be9f502d1df88
hos3ini <seied78@gmail.com>
2022-04-22 23:47:54 +0430

    register

--- src/pages/register.js
+++ src/pages/register.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const Register = () => {
+  const [slideup, setSlideup] = useState(false);
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
   const [email, setEmail] = useState("");
   const [loginInfo, setLoginInfo] = useState({});
   const [signupInfo, setSignupInfo] = useState({});
@@ -12,7 +12,6 @@ const Register = () => {
     setSignupInfo({
       userName,
       password,
-      repeatPassword,
       email,
     });
   };
@@ -22,126 +21,76 @@ const Register = () => {
       password,
     });
   };
+  const toggleSlide = () => {
+    setSlideup(!slideup);
+  };
   return (
-    <>
-      <div class="login-wrap">
-        <div class="login-html">
-          <input id="tab-1" type="radio" name="tab" class="sign-in" checked />
-          <label for="tab-1" class="tab">
-            Sign In
-          </label>
-          <input id="tab-2" type="radio" name="tab" class="sign-up" />
-          <label for="tab-2" class="tab">
-            Sign Up
-          </label>
-          <div class="login-form">
-            <div class="sign-in-htm">
-              <div class="group">
-                <label for="user" class="label">
-                  Username
-                </label>
-                <input
-                  id="user"
-                  type="text"
-                  class="input"
-                  onChange={(e) => setUserName(e.target.value)}
-                />
-              </div>
-              <div class="group">
-                <label for="pass" class="label">
-                  Password
-                </label>
-                <input
-                  id="pass"
-                  type="password"
-                  class="input"
-                  data-type="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div class="group">
-                <input id="check" type="checkbox" class="check" checked />
-                <label for="check">
-                  <span class="icon"></span> Keep me Signed in
-                </label>
-              </div>
-              <div class="group">
-                <input
-                  type="submit"
-                  class="button"
-                  value="Sign In"
-                  onClick={signin}
-                />
-              </div>
-              <div class="hr"></div>
-              <div class="foot-lnk">
-                <a href="#forgot">Forgot Password?</a>
-              </div>
+    <div className="form-container">
+      <div className="form-structor">
+        <div className={slideup ? "signup slide-up" : "signup"}>
+          <h2 className="form-title" onClick={toggleSlide}>
+            <span>or</span>Sign up
+          </h2>
+          <div className="form-holder">
+            <input
+              type="text"
+              className="input"
+              placeholder="Name"
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <input
+              type="email"
+              className="input"
+              placeholder="Email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              className="input"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <button className="submit-btn" onClick={signup}>
+            Sign up
+          </button>
+        </div>
+        <div className={slideup ? "login" : "login slide-up"}>
+          <div className="center">
+            <h2 className="form-title" onClick={toggleSlide}>
+              <span>or</span>Log in
+            </h2>
+            <div className="form-holder">
+              <input
+                type="email"
+                className="input"
+                placeholder="Email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <input
+                type="password"
+                className="input"
+                placeholder="Password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
             </div>
-            <div class="sign-up-htm">
-              <div class="group">
-                <label for="user" class="label">
-                  Username
-                </label>
-                <input
-                  id="user"
-                  type="text"
-                  class="input"
-                  onChange={(e) => setUserName(e.target.value)}
-                />
-              </div>
-              <div class="group">
-                <label for="pass" class="label">
-                  Password
-                </label>
-                <input
-                  id="pass"
-                  type="password"
-                  class="input"
-                  data-type="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div class="group">
-                <label for="pass" class="label">
-                  Repeat Password
-                </label>
-                <input
-                  id="pass"
-                  type="password"
-                  class="input"
-                  data-type="password"
-                  onChange={(e) => setRepeatPassword(e.target.value)}
-                />
-              </div>
-              <div class="group">
-                <label for="pass" class="label">
-                  Email Address
-                </label>
-                <input
-                  id="pass"
-                  type="text"
-                  class="input"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div class="group">
-                <input
-                  type="submit"
-                  class="button"
-                  value="Sign Up"
-                  onClick={signup}
-                />
-              </div>
-              <div class="hr"></div>
-              <div class="foot-lnk">
-                <label for="tab-1">Already Member?</label>
-              </div>
-            </div>
+            <button className="submit-btn" onClick={signin}>
+              Log in
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

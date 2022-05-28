f4eb69105cce1865a7cec206e87dcd9b3de1f6a0
molavi25 <66302842+molavi25@users.noreply.github.com>
2022-05-28 13:05:26 +0430

    add swiper content

--- src/pages/financial/Financial.js
+++ src/pages/financial/Financial.js
@@ -2,14 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import { AnimatedContentMemo } from '../../components/Animated-Content';
 import { FooterMemo } from '../../components/Footer';
-import consult from '../../images/financial-consulting.json';
-import payment from '../../images/secure-card-payment.json';
-import software from '../../images/software.json';
 import team from '../../images/teamwork.json';
 import { ContactMemo } from './Financial-Contact';
 import { FinancialHeaderMemo } from './Financial-Header';
 import { FinancialServicesMemo } from './Financial-Services';
 import { StatsMemo } from './Financial-Stats';
+import { SwiperMemo } from './Swiper';
 
 const Financial = () => {
   const Container = styled.div`
@@ -29,35 +27,7 @@ const Financial = () => {
 
       <ContactMemo></ContactMemo>
 
-      <AnimatedContentMemo
-        header={'Consulting & Development'}
-        desc={
-          'Know-how and experience in the blockchain and financial services industry enable us to offer individual consulting and development services for companies and institutions of all industries and sizes.'
-        }
-        animation={consult}
-        iconWidth={'600px'}
-        iconHeight={'600px'}
-      ></AnimatedContentMemo>
-
-      <AnimatedContentMemo
-        header={'Hardware'}
-        desc={
-          'Our SX1801 payment terminal is the ideal complement to our crypto payment software. It is suitable for any modern form of digital payments as well as for the operation of additional payment software such as cash register systems.'
-        }
-        animation={payment}
-        iconWidth={'600px'}
-        iconHeight={'600px'}
-      ></AnimatedContentMemo>
-
-      <AnimatedContentMemo
-        header={'Software'}
-        desc={
-          'Our digital payment software enables your company to accept digital payments in the form of cryptocurrencies such as Bitcoin or Dash easily and risk-free. With its simplicity, speed and security, it offers both end customers and companies a smooth payment experience.'
-        }
-        animation={software}
-        iconWidth={'600px'}
-        iconHeight={'600px'}
-      ></AnimatedContentMemo>
+      <SwiperMemo></SwiperMemo>
 
       <AnimatedContentMemo
         header={'Our Team'}

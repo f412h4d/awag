92239130057e2454f0873dff0e74237061c902b2
molavi25 <66302842+molavi25@users.noreply.github.com>
2022-05-28 13:17:36 +0430

    bug fixed

--- src/pages/financial/Swiper.js
+++ src/pages/financial/Swiper.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import './Financial.css';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.min.css';

6ef71b82c738a1e3037e8adec015edb820c9c307
Ali Molavi <66302842+molavi25@users.noreply.github.com>
2022-05-29 15:16:25 +0430

    Fixed File Name Typo

---
+++ src/components/Animated-Icon.js
@@ -0,0 +1,40 @@
+import lottie from 'lottie-web';
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+
+const AnimatedIcon = ({ loop, iconWidth, iconHeight, animationData }) => {
+  const ref = useRef(null);
+  const [animation, setAnimation] = useState();
+
+  useEffect(() => {
+    if (ref.current) {
+      const animationItem = lottie.loadAnimation({
+        animationData,
+        loop,
+        autoplay: true,
+        container: ref.current,
+      });
+
+      setAnimation(animationItem);
+    }
+  }, []);
+
+  return (
+    <LottieIcon
+      ref={ref}
+      iconWidth={iconWidth}
+      iconHeight={iconHeight}
+    ></LottieIcon>
+  );
+};
+
+const LottieIcon = styled.div`
+  width: ${({ iconWidth }) => (iconWidth ? `${iconWidth}` : '4.3rem')};
+  height: ${({ iconHeight }) => (iconHeight ? `${iconHeight}` : '3.2rem')};
+
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const AnimatedIconMemo = React.memo(AnimatedIcon);

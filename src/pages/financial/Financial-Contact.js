304c32759f11be8a0b5113455770391d16f6c6a7
molavi25 <66302842+molavi25@users.noreply.github.com>
2022-05-31 00:43:03 +0430

    added scroll animation to contact section with gsap

--- src/pages/financial/Financial-Contact.js
+++ src/pages/financial/Financial-Contact.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { AnimatedIconMemo } from '../../components/Animated-Icon';
 import financialAnalysis from '../../images/financial-analysis-by-employer.json';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Contact = () => {
   const navigate = useNavigate();
 
+  // it dosn't work because scrollbar is not for viewport thats for an element,
+  // it will work after fixing scrollbar
+
+
+  const animate = (boxName, primerx) => {
+    gsap.fromTo(
+      boxName.current,
+      {
+        x: primerx,
+        opacity: 0
+      }, {
+        scrollTrigger: {
+          trigger: containerRef.current,
+          toggleActions: "restart none reverse none"
+        },
+        x: 0,
+        opacity: 1,
+        duration: 1
+    });
+  };
+
+  const containerRef = useRef();
+
+  const picRef = useRef();
+  useEffect(() => {
+    animate(picRef, -100)
+  });
+
+  const textRef = useRef();
+  useEffect(() => {
+    animate(textRef, 100)
+  });
+
   return (
-    <Container>
+    <Container ref={containerRef}>
       <Content>
-        <div>
+        <div ref={picRef}>
           <AnimatedIconMemo
             iconHeight={'400px'}
             iconWidth={'400px'}
@@ -18,8 +55,10 @@ const Contact = () => {
           />
         </div>
 
-        <MainContainer>
-          <Title>BECOME A PIONEER OF THE DIGITAL ECONOMY!</Title>
+        <MainContainer ref={textRef}>
+          <Title>
+            BECOME A PIONEER OF THE DIGITAL ECONOMY!
+          </Title>
 
           <Btn onClick={() => navigate('/contact')}>Contact</Btn>
         </MainContainer>
